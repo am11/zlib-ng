4f5ad02df5feda53f4cbb517943ae619682e6b15
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-12-21 02:37:50 +0200

    .

--- arch/arm/neon_intrins.h
+++ arch/arm/neon_intrins.h
@@ -25,7 +25,7 @@
     out.val[3] = vqsubq_u16(a.val[3], b); \
 } while (0)
 
-#  if defined(__clang__) && defined(__arm__) && !defined(__GNUC__)
+#  if defined(__clang__) && defined(__arm__) && defined(__ANDROID__) && !defined(__GNUC__)
 /* Clang for 32-bit Android and musl-libc have too strict alignment requirement (:256) for x4 NEON intrinsics */
 #    undef ARM_NEON_HASLD4
 #    undef vld1q_u16_x4
